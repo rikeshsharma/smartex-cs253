4bb146a5299b075b2f5c9397f3a9363f53cc0b30
gourav-code <kumarstarsongara@gmail.com>
2023-03-24 10:51:09 +0530

    almost done frontend for histroy page showing data

--- ui/HISTORY.js
+++ ui/HISTORY.js
@@ -1,7 +1,64 @@
+console.log("details.js loadd");
 // get the input field and table
 let searchInput = document.getElementById("search-input");
 let table = document.getElementsByTagName("table")[0];
 
+//const url='https://z3myg583ti.execute-api.ap-south-1.amazonaws.com/default/smartexBE?queryType=getAllMovement';
+function getData(){
+  fetch('https://z3myg583ti.execute-api.ap-south-1.amazonaws.com/default/smartexBE?queryType=getAllMovement', {
+            method: 'POST'
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Handle success or error response from server
+            console.log(data.Items.length);
+            for(let i=0;i<data.Items.length; ++i){
+              if (typeof data.Items[i].address == 'undefined'){
+                document.getElementById("place").innerHTML = '';    
+              }
+              else{
+                document.getElementById("place").innerHTML = data.Items[i].address;
+              }
+              if (typeof data.Items[i].name == 'undefined'){
+                document.getElementById("name").innerHTML = '';
+              }
+              else {
+                document.getElementById("name").innerHTML = data.Items[i].name;
+              }
+              if(typeof data.Items[i].rollno == 'undefined'){
+                document.getElementById("roll").innerHTML = '';    
+              }
+              else {
+                document.getElementById("roll").innerHTML = data.Items[i].rollno;
+              }
+              if( typeof data.Items[i].exitTime == 'undefined'){
+                document.getElementById("outTime").innerHTML = '';
+              }
+              else{
+                document.getElementById("outTime").innerHTML = data.Items[i].exitTime;
+              }
+              if(typeof data.Items[i].roomno == 'undefined'){
+                document.getElementById("room").innerHTML = '';
+              }
+              else{
+                document.getElementById("room").innerHTML = data.Items[i].roomno;
+              }
+              if(typeof data.Items[i].hallno == 'undefined'){
+                document.getElementById("hall").innerHTML = '';
+              }
+              else{
+                document.getElementById("hall").innerHTML = data.Items[i].hallno;
+              }
+            }
+            
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+getData();
+console.log("dfg");
+   
 // add event listener to the input field
 searchInput.addEventListener("keyup", function() {
   let searchText = searchInput.value.toLowerCase();
