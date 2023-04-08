77c7c83284323fae92e3e72b007119c46d03a862
gourav-code <kumarstarsongara@gmail.com>
2023-04-08 17:43:58 +0530

    7April Work of success page complete

--- ui/success.js
+++ ui/success.js
@@ -9,18 +9,23 @@
             let placeOfVisit = document.getElementById('placeInput').value;
 			let phoneNumber = document.getElementById('phoneInput').value;
 			
-			
+			//current time 
+
+2
+			var today = new Date();
+			var time_today = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+			var curr_date = today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate();
 			
 			// Send the data to the server
 			
-			fetch('https://z3myg583ti.execute-api.ap-south-1.amazonaws.com/default/smartexBE?queryType=getUser', {
+			fetch('https://z3myg583ti.execute-api.ap-south-1.amazonaws.com/default/smartexBE?queryType=getExit', {
 				method: 'POST',
 				body: JSON.stringify({
-					name: Name,
-					rollno: rollNo,
-					hallno: hallNo,
-					phonenumber: phoneNumber,
-					placeofvisit: placeOfVisit,
+					//name: Name,
+					rollno: parseInt(rollNo),
+					//hallno: hallNo,
+					//phonenumber: phoneNumber,
+					//placeofvisit: placeOfVisit,
 					
 				}),
 				/*headers: {
@@ -32,14 +37,69 @@
 				// Handle success or error response from server
 				console.log(data);
 				alert('Entry data saved successfully');
+
+				fetch('https://z3myg583ti.execute-api.ap-south-1.amazonaws.com/default/smartexBE?queryType=addMovement', {
+				method: 'POST',
+				body: JSON.stringify({
+					name: Name,
+					rollno: parseInt(rollNo),
+					hallno: hallNo,
+					PhoneNo: phoneNumber,
+					placeofvisit: placeOfVisit,
+					gaurdID: "ABC",
+					time: time_today,
+					date: curr_date
+					
+				}),
+				/*headers: {
+					'Content-Type': 'application/json'
+				}*/
+				})
+				.then(response => response.json())
+				.then(data => {
+					console.log('adding in movement is success ')
+					console.log(data);
+					console.log("fgh");
+				})
+				.catch(error => {
+					console.log("error in addmovement");
+					console.log(error);
+				});
 			})
 			.catch(error => {
 				console.error(error);
+				console.log("Error of get user");
+				fetch('https://z3myg583ti.execute-api.ap-south-1.amazonaws.com/default/smartexBE?queryType=addExit', {
+				method: 'POST',
+				body: JSON.stringify({
+					rollno:	parseInt(rollNo),
+					placeOfVisit: placeOfVisit,
+					gaurdID: "ABC",
+					time: time_today,
+					date: curr_date,
+					PhoneNo: phoneNumber
+					
+				}),
+				/*headers: {
+					'Content-Type': 'application/json'
+				}*/
+				})
+				.then(response => response.json())
+				.then(data => {
+					console.log('add exit data success');
+					console.log(data);
+				})
+				.catch(error => {
+					console.log("error in addexit");
+					console.log(error);
+				});
+
+
                 alert('Error in saving entry data');
 			});
 			
 			// Reset the form
-			document.getElementById('entryForm').reset();
+			// document.getElementById('entryForm').reset();
 		}
 	
 		
