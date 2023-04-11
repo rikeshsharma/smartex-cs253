7114cfde30a78e7beff3a017c3274b45329a1b0a
rikesh <rikesh@iitk.ac.in>
2023-04-11 17:21:37 +0530

    integrated rikesh kgs ajeet

--- ui/success.js
+++ ui/success.js
@@ -9,48 +9,108 @@ document.getElementById('rollnoInput').value = sessionStorage.getItem("studentRo
 document.getElementById('addressInput').value = "Hall: "+sessionStorage.getItem("studentHallno") +" / Room-no: " + sessionStorage.getItem("studentRoomno");
 document.getElementById('phoneInput').value = sessionStorage.getItem("studentPhoneno");
 
-// Define a function to handle  entry form submission
-		function submitForm() {
-			// Get the entry form values
-            let Name = document.getElementById('nameInput').value;
-			document.getElementById('nameInput').value = sessionStorage.getItem("studentName");
-            let rollNo = document.getElementById('rollnoInput').value;
-            let hallNo = document.getElementById('addressInput').value;
-            let placeOfVisit = document.getElementById('placeInput').value;
-			let phoneNumber = document.getElementById('phoneInput').value;
-			
-			
-			
-			// Send the data to the server
-			
-			fetch('https://z3myg583ti.execute-api.ap-south-1.amazonaws.com/default/smartexBE?queryType=addExit', {
-				method: 'POST',
-				body: JSON.stringify({
-					name: Name,
-					rollno: rollNo,
-					hallno: hallNo,
-					// phonenumber: phoneNumber,
-					placeOfVisit: placeOfVisit,
-					gaurdID: gaurdID,
-				}),
-				/*headers: {
-					'Content-Type': 'application/json'
-				}*/
-			})
-			.then(response => response.json())
-			.then(data => {
-				// Handle success or error response from server
-				console.log(data);
-				alert('Entry data saved successfully');
-			})
-			.catch(error => {
-				console.error(error);
-                alert('Error in saving entry data');
-			});
-			
-			// Reset the form
-			document.getElementById('entryForm').reset();
-		}
-	
+
+
 		
+
+// Define a function to handle  entry form submission
+function submitForm() {
+	// Get the entry form values
+	let Name = document.getElementById('nameInput').value;
+	let rollNo = document.getElementById('rollnoInput').value;
+	let hallNo = document.getElementById('addressInput').value;
+	let placeOfVisit = document.getElementById('placeInput').value;
+	let phoneNumber = document.getElementById('phoneInput').value;
+	
+	//current time 
+
+	var today = new Date();
+	var time_today = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+	var curr_date = today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate();
+	
+	// Send the data to the server
+	
+	fetch('https://z3myg583ti.execute-api.ap-south-1.amazonaws.com/default/smartexBE?queryType=getExit', {
+		method: 'POST',
+		body: JSON.stringify({
+			//name: Name,
+			rollno: parseInt(rollNo),
+			//hallno: hallNo,
+			//phonenumber: phoneNumber,
+			//placeofvisit: placeOfVisit,
+			
+		}),
+		/*headers: {
+			'Content-Type': 'application/json'
+		}*/
+	})
+	.then(response => response.json())
+	.then(data => {
+		// Handle success or error response from server
+		console.log(data);
+		alert('Entry data saved successfully');
+
+		fetch('https://z3myg583ti.execute-api.ap-south-1.amazonaws.com/default/smartexBE?queryType=addMovement', {
+		method: 'POST',
+		body: JSON.stringify({
+			name: Name,
+			rollno: parseInt(rollNo),
+			hallno: hallNo,
+			PhoneNo: phoneNumber,
+			placeofvisit: placeOfVisit,
+			gaurdID: "ABC",
+			time: time_today,
+			date: curr_date
+			
+		}),
+		/*headers: {
+			'Content-Type': 'application/json'
+		}*/
+		})
+		.then(response => response.json())
+		.then(data => {
+			console.log('adding in movement is success ')
+			console.log(data);
+			console.log("fgh");
+		})
+		.catch(error => {
+			console.log("error in addmovement");
+			console.log(error);
+		});
+	})
+	.catch(error => {
+		console.error(error);
+		console.log("Error of get user");
+		fetch('https://z3myg583ti.execute-api.ap-south-1.amazonaws.com/default/smartexBE?queryType=addExit', {
+		method: 'POST',
+		body: JSON.stringify({
+			rollno:	parseInt(rollNo),
+			placeOfVisit: placeOfVisit,
+			gaurdID: "ABC",
+			time: time_today,
+			date: curr_date,
+			PhoneNo: phoneNumber
+			
+		}),
+		/*headers: {
+			'Content-Type': 'application/json'
+		}*/
+		})
+		.then(response => response.json())
+		.then(data => {
+			console.log('add exit data success');
+			console.log(data);
+		})
+		.catch(error => {
+			console.log("error in addexit");
+			console.log(error);
+		});
+
+
+		alert('Error in saving entry data');
+	});
+	
+	// Reset the form
+	// document.getElementById('entryForm').reset();
+}
 
