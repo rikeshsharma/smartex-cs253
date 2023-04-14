aa2a55859289e4c10edcdd2a2f0a60e58a44f325
rikesh <rikesh@iitk.ac.in>
2023-04-14 18:15:59 +0530

    fix login null admin-ln guard-ln

--- ui/loginbtn.js
+++ ui/loginbtn.js
@@ -1,6 +1,11 @@
 // Redirect to admin dashboard page
+if(document.getElementById("admin-ln")!=null)
 document.getElementById("admin-ln").addEventListener("click", function(){
     window.location.href = "admin.html";
+  });
+if(document.getElementById("guard-ln")!=null)
+  document.getElementById("guard-ln").addEventListener("click", function(){
+    window.location.href = "index.html";
   });
   
   // Redirect to iit kanpur page
